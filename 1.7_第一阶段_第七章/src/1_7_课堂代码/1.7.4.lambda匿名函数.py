fa6fb1470b1329c2820e52d4329c0d4b5ab19823
AnXin-HITSZ <anxin.hitsz@outlook.com>
2025-11-02 19:09:51 +0800

    Notes - 20251102

---
+++ 1.7_第一阶段_第七章/src/1_7_课堂代码/1.7.4.lambda匿名函数.py
@@ -0,0 +1,15 @@
+"""
+演示 lambda 匿名函数
+"""
+
+# 定义一个函数，接受其他函数输入
+def test_func(compute):
+    result = compute(1, 2)
+    print(f"结果是：{result}")
+
+# 通过 lambda 匿名函数的形式，将匿名函数作为参数传入
+def add(x, y):
+    return x + y
+
+test_func(add)
+test_func(lambda x, y: x + y)
